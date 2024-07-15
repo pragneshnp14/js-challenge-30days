2e79ca29422cae60050b208751383aeda2a8ac8a
pragneshnp14 <pragneshnp1430@gmail.com>
2024-07-15 07:36:01 +0000

    Day-1 challenge done

--- day-1.js
+++ day-1.js
@@ -1,5 +1,75 @@
 // Activity 1
+// Activity 1 : Variable Declaration
 
-
+// Task 1
 var num = 10;
-console.log(num);
+// console.log(num);
+
+// Task 2
+let str = "Hello";
+// console.log(str);
+
+
+// Activity 2
+// Activity 2 : Constant Declaration
+
+// Task 1
+const constant = true;
+// console.log(constant);
+
+// Activity 3
+// Activity-3 : Data Types
+let num1 = 100
+console.log(num1);
+console.log(typeof num1);
+console.log();
+
+let myTeacher ="Hitesh Sir"
+console.log(myTeacher);
+console.log(typeof myTeacher);
+console.log();
+
+let isLoggedIn = true
+console.log(isLoggedIn);
+console.log(typeof isLoggedIn);
+console.log();
+
+let myFriend = {
+    "name": "Human",
+    age: 20,
+    Year: 4,
+    "location": "Deoghar",
+}
+console.log(myFriend);
+console.log(typeof myFriend);
+console.log(typeof myFriend.name);
+console.log(typeof myFriend.age);
+console.log(typeof myFriend.Year);
+console.log(typeof myFriend.location);
+console.log();
+
+let myArr = [1 , 'hey' , 5 , 7 , 9]
+console.log(myArr);
+console.log(typeof myArr);
+console.log(typeof myArr[0]);
+console.log(typeof myArr[1]);
+
+console.log();
+
+
+// Activity-4 : Reassigning Variables
+let Value = 23
+console.log("Reassignment using let ");
+console.log(`Initial value : ${Value}`);
+Value = 25
+console.log(`Final value : ${Value}`);
+
+// Activity - 5 : Understanding const
+const val = 23
+console.log("Reassignment using let ");
+console.log(`Initial value : ${val}`);
+console.log();
+val = 25
+console.log(`Final value : ${val}`);
+
+
